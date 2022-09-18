923487368d3156d5ad1c389773213c2bb537ca89
程坤酱 <chengkun0804@gmail.com>
2022-09-18 22:06:35 +0900

    element 和 router

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h2>Reading Recorder</h2>
+
+    <el-menu mode="horizontal" background-color="#545c64"
+      text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1"><router-link to="/">
+        Home</router-link></el-menu-item>
+      <el-menu-item index="2"><router-link to="/search">
+        Search</router-link></el-menu-item>
+      <el-submenu index="3">
+        <template v-slot:title>Support</template>
+        <el-menu-item index="3-1">
+          <a href="https://github.com/ck76/" target="help">ck-github</a>
+        </el-menu-item>
+        <el-menu-item index="3-2">
+          <a href="https://leetcode.cn/u/ck76-zeujbzmddn/" target="help">ck-leetcode</a>
+        </el-menu-item>
+      </el-submenu>       
+    </el-menu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  width: 800px;
+  margin: 0 auto;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app h2 {
+  color: #009;
+}
+
+#nav {
+  margin-bottom: 15px;
+}
+
+.el-menu-item a {
+  color: #fff;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import installElementPlus from './plugins/element'
+
+const app = createApp(App)
+
+app.config.globalProperties.$http = (url, opts) => fetch(url, opts)
+
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
